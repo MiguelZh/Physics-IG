5ddafa54adeb491d96a756da168991f7f133c433
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2019-02-27 17:35:21 +0100

    feat: texture class added

--- IG1App/Scene.cpp
+++ IG1App/Scene.cpp
@@ -20,12 +20,12 @@ void Scene::init()
   /*grObjects.push_back(new EjesRGB(200.0)); 
   glm::dvec2 vector2d =dvec2(0.0, 0.0);
   grObjects.push_back(new Poliespiral(vector2d, 0, 45, 1, 1, 50));
-  grObjects.push_back(new Dragon(3000));*/
+  grObjects.push_back(new Dragon(3000));
   grObjects.push_back(new TrianguloAnimado(10));
   grObjects.push_back(new Rectangulo(200, 250,0));
   grObjects.push_back(new Rectangulo(50, 50,2));
   grObjects.push_back(new Estrella3D(35.,7., 35.));
-  grObjects.push_back(new Caja(50.));
+  grObjects.push_back(new Caja(50.));*/
 
 
 }
