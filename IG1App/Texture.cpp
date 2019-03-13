1e5176b9bb2ddefffc855545647f27d2dfde06da
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2019-03-13 17:54:36 +0100

    nothing

--- IG1App/Texture.cpp
+++ IG1App/Texture.cpp
@@ -17,7 +17,7 @@ void Texture::bind(GLint modo) { // modo para la mezcla los colores
 }
 void Texture::loadColorBuffer()
 {
-	if (id == 0) init();
+	if (id == 0) { init(); }
 	glReadBuffer(GL_FRONT);
 	glBindTexture(GL_TEXTURE_2D, id); // filters and wrapping
 	glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 0, 0, glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT),0);
